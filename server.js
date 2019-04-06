a761414c8bdade63d1c4ff255295821b418b73fc
Morgan Galvin <morgan@ludwigthepig.com>
2019-04-06 10:11:37 -0600

    added dependencies, ignored .env, and insalled dependencies
    added helmet for security, dotenv for reading env, mongoose for database modeling and communicating

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
+// Dependencies in alphabetical order
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
 const express = require('express');
-const bodyParser = require('body-parser')
+const helmet = require('helmet');
+const mongoose = require('mongoose');
 const path = require('path');
-const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
 
+const app = express(); //getting this party started 🎉
+
+//Configuring dependencies
+dotenv.config();
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(helmet());
+app.use(helmet.frameguard({ action: 'sameorigin' }))
+
+//Routing
 app.get('/ping', function (req, res) {
+  console.log('pong');
  return res.send('pong');
 });
 
@@ -14,5 +26,34 @@ app.get('/', function (req, res) {
 app.get('/login', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'login.html'))
 })
+app.use(function(req, res, next) {
+  res.status(404)
+    .type('text')
+    .send('Not Found');
+});
 
-app.listen(process.env.PORT || 8080);
+//Database set up
+mongoose.connect(process.env.MONGO, (err, db)=>{
+  if (err){
+    console.log(err);
+  } else {
+    console.log('You are now connected to the database!');
+  }
+});
+
+//Setting up the logs
+app.listen(process.env.PORT || 8080, function () {
+  console.log("Listening on port " + process.env.PORT);
+  if(process.env.NODE_ENV==='test') {
+    console.log('Running Tests...');
+    setTimeout(function () {
+      try {
+        runner.run();
+      } catch(e) {
+        const error = e;
+          console.log('Tests are not valid:');
+          console.log(error);
+      }
+    }, 1500);
+  }
+});
